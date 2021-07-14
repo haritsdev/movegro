5df37ff3aa039cc21f7d1566c1481a7e2f5655ce
haritsdev <teguhcodedev1@gmail.com>
2021-07-14 15:54:52 +0700

    PART 3 Show Login By Phone Number

--- lib/UI/Screen/WelcomeScreen.dart
+++ lib/UI/Screen/WelcomeScreen.dart
@@ -2,10 +2,86 @@ import 'package:flutter/material.dart';
 import 'package:movegro/UI/Screen/OnBoarding.dart';
 
 class WelcomeScreen extends StatelessWidget {
-  const WelcomeScreen({Key key}) : super(key: key);
+  bool _validPhoneNumber = false;
 
   @override
   Widget build(BuildContext context) {
+    void showBottomSheet(context) {
+      showModalBottomSheet(
+          isScrollControlled: true,
+          isDismissible: true,
+          context: context,
+          builder: (context) =>
+              StatefulBuilder(builder: (context, StateSetter myState) {
+                return DraggableScrollableSheet(
+                    initialChildSize: 0.75, //set this as you want
+                    maxChildSize: 0.75, //set this as you want
+                    minChildSize: 0.75, //set this as you want
+                    expand: true,
+                    builder: (context, scrollController) {
+                      return Container(
+                          height: MediaQuery.of(context).size.height - 60,
+                          child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Column(
+                                mainAxisSize: MainAxisSize.max,
+                                children: [
+                                  Text('LOGIN',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold)),
+                                  Text('Enter your phone number to process',
+                                      style: TextStyle(fontSize: 12)),
+                                  SizedBox(
+                                    height: 30,
+                                  ),
+                                  TextField(
+                                      decoration: InputDecoration(
+                                          prefixText: '+62',
+                                          labelText: '11 digit mobile number'),
+                                      autofocus: true,
+                                      keyboardType: TextInputType.phone,
+                                      maxLength: 11,
+                                      onChanged: (value) {
+                                        if (value.length == 11) {
+                                          myState(() {
+                                            _validPhoneNumber = true;
+                                          });
+                                        } else {
+                                          myState(() {
+                                            _validPhoneNumber = false;
+                                          });
+                                        }
+                                      }),
+                                  SizedBox(height: 10),
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                        child: AbsorbPointer(
+                                          absorbing:
+                                              _validPhoneNumber ? false : true,
+                                          child: FlatButton(
+                                              color: _validPhoneNumber
+                                                  ? Theme.of(context)
+                                                      .primaryColor
+                                                  : Colors.grey,
+                                              onPressed: () {},
+                                              child: Text(
+                                                  _validPhoneNumber
+                                                      ? 'CONTINUE'
+                                                      : 'ENTER PHONE NUMBER',
+                                                  style: TextStyle(
+                                                      color: Colors.white))),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ]),
+                          ));
+                    });
+              }));
+    }
+
     return Scaffold(
       body: Padding(
           padding: const EdgeInsets.all(20.0),
@@ -42,17 +118,23 @@ class WelcomeScreen extends StatelessWidget {
                   height: 20,
                 ),
                 FlatButton(
-                    onPressed: () {},
-                    child: RichText(
-                      text: TextSpan(
-                          text: 'Already a customer ?',
-                          style: TextStyle(color: Colors.black),
-                          children: [
-                            TextSpan(
-                                text: 'Login',
-                                style: TextStyle(fontWeight: FontWeight.bold))
-                          ]),
-                    ))
+                  onPressed: () {
+                    showBottomSheet(context);
+                  },
+                  child: RichText(
+                    text: TextSpan(
+                        text: 'Already a customer ?',
+                        style: TextStyle(color: Colors.black),
+                        children: [
+                          TextSpan(
+                              text: 'LOGIN',
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.orangeAccent))
+                        ]),
+                  ),
+                ),
               ])
             ],
           )),
